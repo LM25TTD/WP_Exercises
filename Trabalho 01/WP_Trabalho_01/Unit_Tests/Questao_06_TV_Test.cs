74fa97d668715cb17ac55ae2c66b1156ddd14c81
Leandro Bentes <lm25ttd@gmail.com>
2013-09-05 15:16:19 -0400

    Implementacao da Questao 6

---
+++ Trabalho 01/WP_Trabalho_01/Unit_Tests/Questao_06_TV_Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WP_Trabalho_01.Questao06;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public class Questao_06_TV_Test
+    {
+        [TestMethod]
+        public void testVolume_TV()
+        {
+            TV tv = new TV();
+            tv.canal = 50;
+            tv.volume = 50;
+            Assert.AreEqual(50, tv.canal);
+            Assert.AreEqual(50, tv.volume);
+
+            tv.canal = -1;
+            tv.volume = -1;
+            Assert.AreEqual(1, tv.canal);
+            Assert.AreEqual(0, tv.volume);
+
+            tv.canal = 1002;
+            tv.volume = 102;
+            Assert.AreEqual(1000, tv.canal);
+            Assert.AreEqual(100, tv.volume);
+
+        }
+    }
+}
---
+++ Trabalho 01/WP_Trabalho_01/WP_Trabalho_01/Questao06/TV.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WP_Trabalho_01.Questao06
+{
+    public class TV
+    {
+        private int _canal=1;
+        private int _volume=0;
+
+        public int canal
+        {
+            get
+            {
+                return this._canal;
+            }
+            set
+            {
+                this._canal = value;
+                if (value < 1)
+                    this._canal = 1;
+                if (value > 1000)
+                    this._canal = 1000;                
+            }
+        }
+
+        public int volume
+        {
+            get
+            {
+                return this._volume;
+            }
+            set
+            {
+                this._volume = value;
+                if (value < 1)
+                    this._volume = 0;
+                if (value > 100)
+                    this._volume = 100;                
+            }
+        }
+    }
+}
